b6be9c5f8865c95e008b2fc4d27bafa290d97e7a
RachelNS <rachelnelsonschille@gmail.com>
2021-01-18 10:31:16 -0800

    set up routes to save new notes and read saved notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { networkInterfaces } = require("os");
 const path = require("path");
+const fs = require("fs");
 
 // Creates express server
 const app = express();
@@ -18,10 +19,10 @@ app.listen(PORT, function () {
 });
 
 // New note object
-const Note = function(id, title, text){
-    this.id = id;
+const Note = function(title, text, id){
     this.title = title;
     this.text = text;
+    this.id = id;
 };
 
 const savedNotes = [];
@@ -36,3 +37,30 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 
 });
+
+app.post("/api/notes", function( req, res ) {
+    // Create a new note and push it into the saved notes array
+    const newNote = req.body;
+    savedNotes.push(newNote);
+
+    // Each new note will be assigned a unique id based on its position in the saved notes array
+    newNote.id = savedNotes.length+1;
+
+    // Append each new note to db.json file
+    fs.appendFile("db.json", `${JSON.stringify(newNote)}\n`, err => {
+        err ? console.error(err) : console.log("Success!")
+    });
+    return res.json(newNote);
+});
+
+app.get("/api/notes", function ( req, res ) {
+    // Read db.json file and return saved notes
+    fs.readFile("db.json", "utf-8", (error, data) => 
+    error ? console.error(error) : console.log(data)) 
+});
+
+app.delete("/api/notes/:id", function( query ) {
+    // Delete the db.json file
+    fs.unlink("db.json", (err) =>
+    err ? console.error(err) : console.log("File deleted!"))
+})
